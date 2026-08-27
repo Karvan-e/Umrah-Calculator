37956d4ab4c53fecb3f50b77333cad56ac6d6904
Karvan e Asal <karvaneasal@gmail.com>
2026-08-28 01:45:44 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kea-v2.0.1-ui-v1';
+const CACHE='kea-v15.7-final-ui-fix-v4';
 const CORE=['./manifest.json','./assets/logo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
