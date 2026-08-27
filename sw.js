187c89b7ea15a2928b7f8e4aed5682503cd18a3d
Karvan e Asal <karvaneasal@gmail.com>
2026-08-28 00:42:28 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kea-v15.3-sales-online-meezab-date-rates';
+const CACHE='kea-v15.7-hotel-calculator-fix-v3';
 const CORE=['./manifest.json','./assets/logo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
